de876fc77153bb82afcb821f8389dde111d05cab
ajaykumarmohite <ajaykumar.mohite@questionpro.com>
2025-02-25 17:58:24 +0530

    learn session

--- model/Customer.java
+++ model/Customer.java
@@ -7,21 +7,9 @@ import support.constant.CustomerType;
 public class Customer {
     private String name;
     private CustomerType type;
-    private double discount;
 
     public Customer(String name, CustomerType type) {
         this.name = name;
         this.type = type;
-        setDiscount();
-    }
-
-    public void setDiscount() {
-        if (type == CustomerType.REGULAR) {
-            discount = 0.05;
-        } else if (type == CustomerType.PREMIUM) {
-            discount = 0.1;
-        } else (type == CustomerType.VIP) {
-            discount = 0.2;
-        }
     }
 }
